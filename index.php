9cca4621bedd38eb8c19968729f00aa49f181144
Petter Hareim <phareim@gmail.com>
2013-09-17 07:17:14 -0400

    art

--- index.php
+++ index.php
@@ -16,19 +16,34 @@
             canvas.width = document.getElementsByTagName('body')[0].clientWidth-10;
             canvas.height =  window.innerHeight-20;
             
-            var mySprite = {
+            
+            var mySprite = new_cherry();
+            mySprite.width = 25;
+            mySprite.height = 25;
+            mySprite.speed = 150;
+            /*{
                 x: 100,
                 y: 100,
                 width: 25,
                 height: 25,
                 speed: 200,
                 color: '#c00'
-            };
+            };*/
+            
+            var cherries = [];
+            
+            var i=0;
+            while(i <= 350){
+                cherries[i] = new_cherry();
+                i++;
+            }
+            cherries[i] = mySprite;
             
             function new_cherry(){
                 return{
-                    x: ((Math.random()+0.3)*100)+20,
-                    y: ((Math.random()+0.3)*100)+20,
+                    x: Math.round(((Math.random()*document.getElementsByTagName('body')[0].clientWidth))/6)
+                    +document.getElementsByTagName('body')[0].clientWidth/4,
+                    y: Math.round(((Math.random()*window.innerHeight)/6))+window.innerHeight/4,
                     width: 10,
                     height: 10,
                     color: 
@@ -50,30 +65,38 @@
                     },
                     
                     changeDirection:function()
-                    {
-                        if( this.x <0 || this.x >= canvas.width-(this.width))
+                    {   
+                        if( this.x <=0) 
                         {
-                            this.direction.right = !this.direction.right;
+                            this.direction.right = true;
                         }
-                        if(this.y <0 || this.y >= canvas.height-(this.height))
+                        if(this.x >= canvas.width-(this.width))
                         {
-                            this.direction.up = !this.direction.up;
+                            this.direction.right = false;
+                        }
+                        if(this.y <=0)
+                        {
+                            this.direction.up = true;
+                        }
+                        if(this.y >= canvas.height-(this.height))
+                        {
+                            this.direction.up = false;
                         }
                         return this;
                     }
                 };
             }
             
-            var cherries = [];
-            
-            
-            var i=0;
-            while(i <= 700){
-                console.log(i);
-                cherries[i] = new_cherry() ;
-                i++;
+            function intersects(block1,block2) {
+                block2.width += block2.x;
+                block1.width += block1.x;
+                if (block2.x > block1.width || block1.x > block2.width) return false;
+                block2.height += block2.y;
+                block1.heigth += block1.y;
+                if (block2.y > block1.heigth || block1.y > block2.heigth) return false;
+              return true;
             }
-            
+
             var keysDown = {};
             window.addEventListener('keydown', function(e) {
                 keysDown[e.keyCode] = true;
@@ -91,18 +114,18 @@
             
             function update(mod) {
                 if (37 in keysDown && mySprite.x>0) {
-                    mySprite.x -= mySprite.speed * mod;
+                    mySprite.x -= mySprite.speed * mod*1.2;
                     
                 }
                 if (38 in keysDown && mySprite.y>0) {
-                    mySprite.y -= mySprite.speed * mod;
+                    mySprite.y -= mySprite.speed * mod*1.2;
                     
                 }
                 if (39 in keysDown && mySprite.x < (canvas.width-(mySprite.width))) {
-                    mySprite.x += mySprite.speed * mod;
+                    mySprite.x += mySprite.speed * mod*1.2;
                 }
                 if (40 in keysDown && mySprite.y < (canvas.height-(mySprite.height))) {
-                    mySprite.y += mySprite.speed * mod;
+                    mySprite.y += mySprite.speed * mod*1.2;
                 }
             }
             
@@ -122,10 +145,13 @@
                 update((Date.now() - time) / 1000);
                
                 cherries.forEach(function(a,b,c){
-                    updateCherry(a,(Date.now() - time) / 1000);
-                    
+                    updateCherry(a,(Date.now() - time) / 1000); 
                 });
                 
+                cherries.filter(function(c){
+                    /*if(c.x <= 0)
+                        console.log(c.x+" "+c.y);*/
+                });
                 
                 render();
                 time = Date.now();
